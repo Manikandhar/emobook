92100269c53ccf465a143b780625187dd3a6edde
Manikandhar <mani.kandhar38@outlook.com>
2015-07-19 17:14:49 +0530

    First commit

---
+++ assets/js/spa.js
@@ -0,0 +1,19 @@
+angular.module('Platzi',[]);
+angular.module('Platzi').controller('BaseCtrl', ['$scope', function($scope){
+	console.log('Base controller called.');
+	
+	io.socket.get('/emoji', function(data){
+		$scope.emojis = data;
+		$scope.$apply();
+	});
+	
+	io.socket.on('emoji', function(event){
+		switch(event.verb){
+			case 'created':
+			$scope.emojis.push(event.data);
+			$scope.$apply();
+			break;
+		}
+	});
+	
+}]);
